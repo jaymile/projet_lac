086f6abbcc8ff322d3607584574a8d18f04e52c8
aminou aminou sam djemil <djemil.aminou@gmail.com>
2021-11-17 00:08:30 +0000

    modif dans authenticator et ajout de bundle

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -52,7 +52,8 @@ class Authenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
     }
 
     protected function getLoginUrl(Request $request): string
